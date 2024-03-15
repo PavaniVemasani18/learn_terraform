3a09057678edb1c4b1b210f99a416692fc753c00
Pavani <vemasani.pavani@gmail.com>
2024-03-15 16:53:01 +0530

    create a resource through terraform

--- test.tf
+++ test.tf
@@ -1,4 +1,8 @@
-resource "aws_instance" "test" {
-  ami           = "ami-05f020f5935e52dc4"
-  instance_type = "t3.micro"
-}
+//resource "aws_instance" "page" {
+//  ami           = "ami-05f020f5935e52dc4"
+//  instance_type = "t3.micro"
+//  vpc_security_group_ids = "[sg-048045238f3c8089f]"
+//  tags = {
+//    Name = "frontend"
+//  }
+//}
