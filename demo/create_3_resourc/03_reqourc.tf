23741d7abd7ad2ebe76075d9d06adfb196eb9aec
Pavani <vemasani.pavani@gmail.com>
2024-03-22 20:10:00 +0530

    insert name and security groups

---
+++ demo/create_3_resourc/03_reqourc.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "frontend" {
+  ami = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [sg-0972bb06b91851db8]
+  tags = {
+    Name = frontend
+  }
+}
