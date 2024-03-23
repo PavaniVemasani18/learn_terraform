bbc2adda0720c78f16f87fb0ee3722a97c97f688
Pavani <vemasani.pavani@gmail.com>
2024-03-23 16:21:14 +0530

    label name for instance for creating new instances

--- modules/demo/02-res.tf
+++ modules/demo/02-res.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "test" {
+  ami=var.ami
   instance_type = var.instance_type
   tags = {
     Name = var.name
   }
 }
 
-
+variable ami{}
 variable "instance_type"{}
 variable "name"{}
 
--- modules/main.tf
+++ modules/main.tf
@@ -1,11 +1,13 @@
 module "demo-1" {
   source = "./demo"
+  ami = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
   name = "demo1"
 
 }
 module "demo-2" {
   source = "./demo"
+  ami = "ami-05f020f5935e52dc4"
   instance_type = "t3.micro"
   name = "demo2"
 }
