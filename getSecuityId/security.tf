efcda2c5a8584ab5043c7a46dcd7e3bcf035aabb
Pavani <vemasani.pavani@gmail.com>
2024-03-23 14:32:38 +0530

    create variable and load security group into tfvars file

--- getSecuityId/security.tf
+++ getSecuityId/security.tf
@@ -1,6 +1,12 @@
 data "aws_security_group" "lb_sg" {
   name = "allow-all"
+  id="sg-0972bb06b91851db8"
 }
 output "security_group" {
   value = data.aws_security_group.lb_sg
+
+}
+variable "id" {}
+output "sec_grp_id" {
+  value = var.id
 }
